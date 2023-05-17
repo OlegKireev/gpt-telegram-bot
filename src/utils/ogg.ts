5c6d8d7e066633173b0ec200aa33b82084701cc8
Oleg Kireev <okireev@ubic.tech>
2023-05-17 11:05:27 +0400

    @/utils/ogg: implement create method

---
+++ src/utils/ogg.ts
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { createWriteStream } from 'fs';
+import { resolve } from 'path';
+import { createDirectoryIfNotExists } from './dir';
+
+class OggConverter {
+  // constructor() {
+
+  // }
+
+  // toMp3() {
+
+  // }
+
+  // eslint-disable-next-line class-methods-use-this
+  async create(url: string, filename: string) {
+    try {
+      await createDirectoryIfNotExists('../temp');
+      const oggPath = resolve(__dirname, '../temp', `${filename}.ogg`);
+      // eslint-disable-next-line no-undef
+      const response = await axios.get<NodeJS.ReadableStream>(url, {
+        responseType: 'stream',
+      });
+      return new Promise((res) => {
+        const stream = createWriteStream(oggPath);
+        response.data.pipe(stream);
+        stream.on('finish', () => res(oggPath));
+      });
+    } catch (err) {
+      console.error('Error while creating OGG', err);
+    }
+  }
+}
+
+export const ogg = new OggConverter();
