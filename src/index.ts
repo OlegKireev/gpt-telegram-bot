8423d2f030f7300585f83469381f8aea2c812f9f
Oleg Kireev <okireev@ubic.tech>
2023-05-17 09:33:29 +0400

    @/index: create and start telegram bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
+import { Telegraf } from 'telegraf';
+import { TELEGRAM_BOT_TOKEN } from './constants';
+
+const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+
 const bootstrap = () => {
-  console.log('working...');
+  bot.launch();
+  console.log('Bot started!');
 };
 
 bootstrap();
+
+process.once('SIGINT', () => bot.stop('Node SIGINT event'));
+process.once('SIGTERM', () => bot.stop('Node SIGTERM event'));
