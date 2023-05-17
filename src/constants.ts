91c41746d67f8a7a04111b11ba631454d64b2d31
Oleg Kireev <okireev@ubic.tech>
2023-05-17 09:30:39 +0400

    @/constants: export GPT_API_KEY, TELEGRAM_BOT_TOKEN

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,5 @@ dotenv.config();
 export const ROOT = path.resolve(__dirname, '..');
 
 export const { PORT } = process.env;
+export const { GPT_API_KEY } = process.env;
+export const { TELEGRAM_BOT_TOKEN } = process.env;
