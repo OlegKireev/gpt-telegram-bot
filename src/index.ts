d078fa36986c3a9e663d8dec30fe5a3818ac74cb
Oleg Kireev <okireev@ubic.tech>
2023-05-17 09:51:33 +0400

    @/index: send a voice file link on a voice message sends

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters';
 import { TELEGRAM_BOT_TOKEN } from './constants';
 
 const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
@@ -8,6 +9,20 @@ const bootstrap = () => {
   console.log('Bot started!');
 };
 
+bot.on(message('voice'), async (ctx) => {
+  try {
+    const userId = ctx.message.from.id;
+    const fileId = ctx.message.voice.file_id;
+    const link = (await ctx.telegram.getFileLink(fileId)).href;
+    ctx.reply(link);
+  } catch (err) {
+    console.error('Error while voice message', err);
+  }
+});
+
+bot.command('start', async (ctx) => {
+  'Добро пожаловать в Chat GPT voice бот.';
+});
 bootstrap();
 
 process.once('SIGINT', () => bot.stop('Node SIGINT event'));
