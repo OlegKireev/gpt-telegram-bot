b020f6a2cdc030b1e75ed4d6e1e1adb1b6aebc2c
Oleg Kireev <okireev@ubic.tech>
2023-05-18 18:00:26 +0400

    index: create session context

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
-import { Telegraf } from 'telegraf';
+import { type Context, Telegraf, session } from 'telegraf';
 import { message } from 'telegraf/filters';
 import { code } from 'telegraf/format';
+import { type ChatCompletionRequestMessage } from 'openai';
 import { TELEGRAM_BOT_TOKEN } from './constants';
 import { OggConverter } from './utils/ogg';
 import { OpenAI } from './utils/open-ai';
 
-const bot = new Telegraf(TELEGRAM_BOT_TOKEN);
+interface ISessionContext extends Context {
+  session: {
+    messages: ChatCompletionRequestMessage[];
+  };
+}
+
+const INITIAL_SESSION = {
+  messages: [],
+};
+
+const bot = new Telegraf<ISessionContext>(TELEGRAM_BOT_TOKEN);
 const openAi = new OpenAI();
 
-const bootstrap = () => {
-  bot.launch();
-};
+bot.use(session());
 
 bot.on(message('voice'), async (ctx) => {
   try {
@@ -33,9 +42,22 @@ bot.on(message('voice'), async (ctx) => {
   }
 });
 
-bot.command('start', async () => {
-  'Добро пожаловать в Chat GPT voice бот.';
+bot.command('start', async (ctx) => {
+  // eslint-disable-next-line no-param-reassign
+  ctx.session = INITIAL_SESSION;
+  ctx.reply('Добро пожаловать в Chat GPT voice бот.');
 });
+
+bot.command('new', async (ctx) => {
+  // eslint-disable-next-line no-param-reassign
+  ctx.session = INITIAL_SESSION;
+  ctx.reply(code('Я забыл о чем мы говорили прежде'));
+});
+
+const bootstrap = () => {
+  bot.launch();
+};
+
 bootstrap();
 
 process.once('SIGINT', () => bot.stop('Node SIGINT event'));
