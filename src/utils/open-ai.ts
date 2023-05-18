5f77124613650b27b07c96b6c5f2a3b6921f3414
Oleg Kireev <okireev@ubic.tech>
2023-05-18 16:39:25 +0400

    open-ai: implement transcript logic

--- src/utils/open-ai.ts
+++ src/utils/open-ai.ts
@@ -1,4 +1,6 @@
+import { createReadStream } from 'fs';
 import { Configuration, OpenAIApi } from 'openai';
+import { isAxiosError } from 'axios';
 import { GPT_API_KEY } from '../constants';
 
 export class OpenAI {
@@ -18,10 +20,17 @@ export class OpenAI {
     });
   }
 
-  // eslint-disable-next-line class-methods-use-this
   async transcript(filepath: string): Promise<string> {
-    return new Promise((res) => {
-      res('');
-    });
+    try {
+      const file = await createReadStream(filepath);
+      // @ts-expect-error
+      const response = await this._api.createTranscription(file, 'whisper-1');
+      return response.data.text;
+    } catch (err) {
+      if (isAxiosError(err)) {
+        console.error(err.response);
+      }
+      throw new Error(`Error while transcript mp3 file to text, ${err}`);
+    }
   }
 }
