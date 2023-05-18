967e228f1301b226f72a5f68316bbd4603239e83
Oleg Kireev <okireev@ubic.tech>
2023-05-18 09:36:29 +0400

    @/utils/dir: create deleteFile

--- src/utils/dir.ts
+++ src/utils/dir.ts
@@ -1,9 +1,17 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
 
 export const createDirectoryIfNotExists = (dirName: string) => {
   const absolutePath = path.join(__dirname, dirName);
   if (!fs.existsSync(absolutePath)) {
     fs.mkdirSync(absolutePath);
   }
+};
+
+export const deleteFile = async (filepath: string) => {
+  await fs.unlink(filepath, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
 };
