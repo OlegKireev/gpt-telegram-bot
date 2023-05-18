9c9ae7e9b6b60dc5be3ec2e2c7599377cc6c6e60
Oleg Kireev <okireev@ubic.tech>
2023-05-18 09:47:28 +0400

    @/utils/open-ai: create OpenAI class

---
+++ src/utils/open-ai.ts
@@ -0,0 +1,16 @@
+export class OpenAI {
+  // constructor() {}
+  // eslint-disable-next-line class-methods-use-this
+  async chat(text: string): Promise<string> {
+    return new Promise((res) => {
+      res('');
+    });
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async transcript(filepath: string): Promise<string> {
+    return new Promise((res) => {
+      res('');
+    });
+  }
+}
