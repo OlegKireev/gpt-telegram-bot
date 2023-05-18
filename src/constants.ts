fa8a6b7ea64c02aefa2b0c2258f7d632890f2fc7
Oleg Kireev <okireev@ubic.tech>
2023-05-18 09:23:30 +0400

    @/constants: add TEMP_FILES_PATH

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
-import path from 'path';
+import path, { resolve } from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const ROOT = path.resolve(__dirname, '..');
+export const ROOT = path.resolve(__dirname);
 
 export const { PORT } = process.env;
 export const { GPT_API_KEY } = process.env;
 export const { TELEGRAM_BOT_TOKEN } = process.env;
+export const TEMP_FILES_PATH = resolve(ROOT, 'temp');
