87edb20073f350840a9d1a678e377e832ad97de2
Oleg Kireev <okireev@ubic.tech>
2023-05-19 10:23:09 +0400

    @/events/voice: move logic from index file

---
+++ src/events/voice.ts
@@ -0,0 +1,41 @@
+import { code } from 'telegraf/format';
+import { message } from 'telegraf/filters';
+import { OggConverter } from '../classes/ogg';
+import { INITIAL_SESSION } from '../constants';
+import { bot, openAi } from '../instance';
+
+bot.on(message('voice'), async (ctx) => {
+  // eslint-disable-next-line no-param-reassign
+  ctx.session ??= INITIAL_SESSION;
+  try {
+    await ctx.reply(code('Загрузка...'));
+    const userId = String(ctx.message.from.id);
+    const fileId = ctx.message.voice.file_id;
+    const link = (await ctx.telegram.getFileLink(fileId)).href;
+    const ogg = new OggConverter(userId);
+
+    const oggPath = await ogg.create(link);
+    const mp3Path = await ogg.toMp3(oggPath);
+
+    const text = await openAi.transcript(mp3Path);
+    await ctx.reply(code(`Твой запрос: ${text}`));
+    await ogg.deleteFile('mp3');
+
+    const { messages } = ctx.session;
+    messages.push({
+      content: text,
+      role: 'user',
+    });
+
+    await ctx.reply(code(`Надо подумать...`));
+    const answer = await openAi.chat(ctx.session.messages);
+
+    messages.push({
+      role: 'assistant',
+      content: answer,
+    });
+    ctx.reply(answer);
+  } catch (err) {
+    console.error('Error while voice message', err);
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,11 @@
 /* eslint-disable no-param-reassign */
-import { Telegraf, session } from 'telegraf';
-import { message } from 'telegraf/filters';
+import { session } from 'telegraf';
 import { code } from 'telegraf/format';
-import { TELEGRAM_BOT_TOKEN } from './constants';
-import { OggConverter } from './utils/ogg';
-import { OpenAI } from './utils/open-ai';
-import { type ISession, type ISessionContext } from './definitions/telegraf';
-
-const INITIAL_SESSION: ISession = {
-  messages: [],
-};
-
-const bot = new Telegraf<ISessionContext>(TELEGRAM_BOT_TOKEN);
-const openAi = new OpenAI();
+import { INITIAL_SESSION } from './constants';
+import { bot } from './instance';
+import './events/voice';
 
 bot.use(session());
-
-bot.on(message('voice'), async (ctx) => {
-  ctx.session ??= INITIAL_SESSION;
-  try {
-    await ctx.reply(code('Загрузка...'));
-    const userId = String(ctx.message.from.id);
-    const fileId = ctx.message.voice.file_id;
-    const link = (await ctx.telegram.getFileLink(fileId)).href;
-    const ogg = new OggConverter(userId);
-
-    const oggPath = await ogg.create(link);
-    const mp3Path = await ogg.toMp3(oggPath);
-
-    const text = await openAi.transcript(mp3Path);
-    await ctx.reply(code(`Твой запрос: ${text}`));
-    await ogg.deleteFile('mp3');
-
-    const { messages } = ctx.session;
-    messages.push({
-      content: text,
-      role: 'user',
-    });
-
-    await ctx.reply(code(`Надо подумать...`));
-    const answer = await openAi.chat(ctx.session.messages);
-
-    messages.push({
-      role: 'assistant',
-      content: answer,
-    });
-    ctx.reply(answer);
-  } catch (err) {
-    console.error('Error while voice message', err);
-  }
-});
 
 bot.command('start', async (ctx) => {
   ctx.session = INITIAL_SESSION;
