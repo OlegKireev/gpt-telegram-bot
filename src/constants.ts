f33afae2795076dd21aaf6997233cf31dac09c64
Oleg Kireev <okireev@ubic.tech>
2023-05-19 10:53:31 +0400

    @/constants: organize constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,11 +5,11 @@ import { type ISession } from './definitions/telegraf';
 dotenv.config();
 
 export const ROOT = path.resolve(__dirname);
+export const TEMP_FILES_PATH = resolve(ROOT, 'temp');
 
 export const { PORT } = process.env;
 export const { GPT_API_KEY } = process.env;
 export const { TELEGRAM_BOT_TOKEN } = process.env;
-export const TEMP_FILES_PATH = resolve(ROOT, 'temp');
 
 export const INITIAL_SESSION: ISession = {
   messages: [],
