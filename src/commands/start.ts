d02854c508cd9b3e16f4793b06395c1f303582ff
Oleg Kireev <okireev@ubic.tech>
2023-05-19 19:14:23 +0400

    @/commands/start: change welcome message

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,4 +3,7 @@ import { bot } from '../instance';
 export const start = bot.command('start', async (ctx) => {
   // eslint-disable-next-line no-param-reassign
   ctx.session = { messages: [] };
+  ctx.reply(
+    'Привет! Я рад приветствовать тебя в моем чате. Я ChatGPT бот, и я здесь, чтобы помочь тебе найти ответы на вопросы и поддержать тебя в любых ситуациях. Давай начнем наше общение! Для того чтобы я забыл предыдущие сообщения выбери команду /reset в меню',
+  );
 });
